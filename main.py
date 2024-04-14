9b66b7dcd068bae0e07ddf2cb18c1223e9342c83
yash-raj202134 <yashraj3376@gmail.com>
2024-04-14 21:48:26 +0530

    app main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
-
+from backend.main import *
 app = FastAPI()
 
 
@@ -13,3 +13,29 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+
+
+
+
+@app.post("/")
+async def handle_request(request: Request):
+    # Retrieve the JSON data from the request
+    payload = await request.json()
+
+    # Extract the necessary information from the payload
+    # based on the structure of the WebhookRequest from Dialogflow
+    intent = payload['queryResult']['intent']['displayName']
+    parameters = payload['queryResult']['parameters']
+    output_contexts = payload['queryResult']['outputContexts']
+    session_id = generic_helper.extract_session_id(output_contexts[0]["name"])
+
+    intent_handler_dict = {
+        'order.add - context: ongoing-order': add_to_order,
+        'order.remove - context: ongoing-order': remove_from_order,
+        'order.complete - context: ongoing-order': complete_order,
+        'track.order - context: ongoing-tracking': track_order
+    }
+
+    return intent_handler_dict[intent](parameters, session_id)
